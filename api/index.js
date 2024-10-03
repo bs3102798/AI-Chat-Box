0af6cc1ae3843b1de44eafb77362055cc2105d28
Burness Scott III <bsiii102798@gmail.com>
2024-10-02 18:43:56 -0700

    chatLayOut

---
+++ api/index.js
@@ -0,0 +1,6 @@
+
+
+const PromptInput = document.getElementById('promptInput');
+const generateBtn = document.getElementById('generateBtn');
+const StopBtn = document.getElementById('StopeBtn');
+const resultText = document.getElementById('resultText');
